10151470cfef4539c605b3f7fb080dce96db9e5d
Antonio Flores <jesusantoniofloresbriones@gmail.com>
2025-09-20 11:21:34 -0700

    Primer commit: proyecto inicial con cookiecutter-data-science

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Análisis de la matrícula de escuelas públicas y privadas en Hermosillo para estudiar desigualdad educativa.',
+    author='MCD_desigualdad',
+    license='MIT',
+)
